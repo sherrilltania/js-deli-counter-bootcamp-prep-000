48b49056994798c0fcdc2b1aab3ccdbe6069d199
Tatiana Sherrill <anilorac_sol@yahoo.com>
2018-07-26 11:52:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var katzDeli = [];
+var katzDeliLine = [];
 
-function takeANumber(katzDeli, name){
-  katzDeli.push(`${name}`)
-  return (`Welcome, ${name}. You are number ${katzDeli.length} in line.`)
+function takeANumber(katzDeliLine, name){
+  katzDeliLine.push(`${name}`)
+  return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
 }
 
 
